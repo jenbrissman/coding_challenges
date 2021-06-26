7aa7afa6f2e7d4b2f9db7e3c6dbd5a40fe11c325
Jen Brissman <brissman514@gmail.com>
2021-06-25 17:20:01 -0700

    maxstack

--- stacks.py
+++ stacks.py
@@ -1,24 +1,93 @@
-###** Bounded Ratio **###
-# You are given an array of integers a and two integers l and r. You task is to calculate a boolean array b, where b[i] = true if there exists an integer x, such that a[i] = (i + 1) * x and l ≤ x ≤ r. Otherwise, b[i] should be set to false.
+# Implement the MaxStack class:
 
-# Example
-
-# For a = [8, 5, 6, 16, 5], l = 1, and r = 3, the output should be boundedRatio(a, l, r) = [false, false, true, false, true].
-
-# For a[0] = 8, we need to find a value of x such that 1 * x = 8, but the only value that woul
-#print(boundedRatio([8, 5, 6, 16, 5], 1, 3)) # => [false, false, true, false, true]
+# MaxStack() initializes the stack object.
+# void push(val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMax() retrieves the maximum element in the stack.
 
 
-# For a[0] = 8, we need to find a value of x such that 1 * x = 8, but the only value that would work is x = 8 which doesn't satisfy the boundaries 1 ≤ x ≤ 3, so b[0] = false.
-# For a[1] = 5, we need to find a value of x such that 2 * x = 5, but there is no integer value that would satisfy this equation, so b[1] = false.
-# For a[2] = 6, we can choose x = 2 because 3 * 2 = 6 and 1 ≤ 2 ≤ 3, so b[2] = true.
-# For a[3] = 16, there is no an integer 1 ≤ x ≤ 3, such that 4 * x = 16, so b[3] = false.
-# For a[4] = 5, we can choose x = 1 because 5 * 1 = 5 and 1 ≤ 1 ≤ 3, so b[4] = true.
+class Maxstack:
+    def __init__(self):
+        self.og_stack = []
+        self.max_stack = []
+    
+    def push(self, x):
+        self.og_stack.append(x)
+        if len(self.max_stack)==0:
+            self.max_stack.append(x)
+        else:
+            if self.max_stack[-1]<x:
+                self.max_stack.append(x)
+            else:
+                self.max_stack.append(self.max_stack[-1])
+                    
+    def pop(self):
+        if len(self.og_stack):
+            self.og_stack.pop()
+            self.max_stack.pop()
+        return None
 
-def boundedRatio(a, l, r):
-    b=[]
-    for i in range(len(a)):
-        x = a[i] / (i + 1)
-        b.append(l <= x <= r and x== int(x))
-    return b
-print(boundedRatio([8, 5, 6, 16, 5], 1, 3))
+    def top(self):
+        return self.og_stack[-1]
+
+    def getMax(self):
+        return self.max_stack[-1]
+
+test = Maxstack()
+test.push(2)
+test.push(5)
+test.push(3)
+test.push(10)
+test.push(12)
+test.pop()
+test.pop()
+test.push(1000)
+test.push(3)
+test.push(-1)
+test.push(8)
+print(test.getMax())
+
+print(test.og_stack)
+print(test.max_stack)
+
+
+
+
+
+
+# class Stack():
+
+#     def __init__(self, list=[]):
+#         print('init')
+#         self._items = list
+
+#     def isEmpty(self):
+#         print('isEmpty')
+#         return not self._items
+      
+#     def push(self, item):
+#         print('push')
+#         self._items.append(item)
+
+#     def pop(self):
+#         print('pop')
+#         return self._items.pop()
+
+#     def size(self):
+#         print('size')
+#         return len(self._items)
+      
+
+#     def peek(self):
+#         print('peek')
+#         return self._items[-1]
+      
+#       #testing
+# stack = Stack([1, 2, 3, 4, 5])
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.peek())
+# print(stack.peek())
+
+
