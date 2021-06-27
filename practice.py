49814eb66880efe1797a769241f69053f1165986
Jen Brissman <brissman514@gmail.com>
2021-06-26 20:43:03 -0700

    two sum

--- practice.py
+++ practice.py
@@ -22,3 +22,28 @@ def boundedRatio(a, l, r):
         b.append(l <= x <= r and x== int(x))
     return b
 print(boundedRatio([8, 5, 6, 16, 5], 1, 3))
+
+
+
+#Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]: 
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+       
+        seen = {}
+
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target-num], i]
+            elif num not in seen:
+                seen[num]=i
+        
